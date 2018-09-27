a4e53651cea5bfcb372813a8b92393356f80ef8d
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-09-27 19:16:44 +0200

    Added a data access layer for topics

---
+++ src/main/java/com/mbalazs9001/springdataapidemo/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.mbalazs9001.springdataapidemo.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+}
