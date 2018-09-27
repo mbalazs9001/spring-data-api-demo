1e0dbd58a38b589659fcac3605a7303b5a575954
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-09-27 14:41:01 +0200

    Initial commit

---
+++ src/main/java/com/mbalazs9001/springdataapidemo/SpringDataApiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.mbalazs9001.springdataapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApiDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mbalazs9001/springdataapidemo/SpringDataApiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mbalazs9001.springdataapidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataApiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
